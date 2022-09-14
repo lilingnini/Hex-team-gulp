1ee7b0da45dd15bc4223dfef1d05cbaac1d41a9e
Nini <ninistyle1107@gmail.com>
2022-09-14 23:43:12 +0800

    Update 2022-09-14T15:43:05.203Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,58 +3,24 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
-var swiper = new Swiper(".learn-swiper", {
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
   autoplay: {
-    delay: 1200,
+    delay: 2500,
     disableOnInteraction: false
   },
-  speed: 1200,
-  loop: true,
-  spaceBetween: 24,
-  slidesPerView: 3,
-  //多欄
-  breakpoints: {
-    768: {
-      slidesPerView: 6
-    },
-    992: {
-      slidesPerView: 9
-    }
-  }
-});
-var swiper2 = new Swiper('.feedback-swiper', {
-  // Optional parameters
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true
-  },
-  // If we need pagination
   pagination: {
-    el: '.swiper-pagination'
+    el: ".swiper-pagination",
+    clickable: true
   },
-  // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
   }
-});
-var swiper = new Swiper(".lesson-swiper", {
-  autoplay: {
-    delay: 1200,
-    disableOnInteraction: false
-  },
-  speed: 1200,
-  loop: true,
-  spaceBetween: 16,
-  slidesPerView: 3,
-  //多欄
-  breakpoints: {
-    768: {
-      slidesPerView: 3
-    },
-    992: {
-      slidesPerView: 6
-    }
-  }
+}); // 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
+
+$('.row').imagesLoaded().progress(function () {
+  $('.row').masonry(); // 渲染整體畫面
 });
 //# sourceMappingURL=all.js.map
